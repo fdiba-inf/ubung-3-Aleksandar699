7731af8eec633e82021aa737f08ef504d76a9e55
Aleksandar699 <aleksandar.mihaylov@fdiba.tu-sofia.bg>
2020-10-17 15:19:03 +0000

    ubung3

--- src/exercise3/PrimeNumber.java
+++ src/exercise3/PrimeNumber.java
@@ -1,23 +1,23 @@
 package exercise3;
+
 import java.util.Scanner;
-public class PrimeNumber {
-public static void main (String [] args){
-Scanner input = new Scanner(System.in);
 
-int number = input.nextInt();
-int d=2;
-boolean prime = true;
-while (d<number) {
+public class PrimeNumber{
+  
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
 
-if (number % d ==0) {
-  prime=false;
-  break;
+    int number = input.nextInt();
+    int b = 2;
+    boolean Prime = true;
+    while(b<number){
+      if(number%b==0){
+        Prime=false;
+      }
+      b++;
+    }
+    System.out.println("Prime number: " + Prime);
+  }
 }
-d++;
-}
-System.out.println("prime number: " + prime);
-}
-}
-
 
 
